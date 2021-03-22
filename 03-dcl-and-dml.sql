93ca019d7fa565255c1d1e7a8155997026867369
ltk <xorud2592@gmail.com>
2021-03-22 21:15:35 +0900

    Sequnce

--- 03-dcl-and-dml.sql
+++ 03-dcl-and-dml.sql
@@ -1,33 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@xorud2592 
-javaweb-bit
-/
-SQL
-1
-00
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-SQL/03-dcl-and-dml.sql
-@namsk
-namsk 사용자 관리
-Latest commit a22fb29 1 hour ago
- History
- 1 contributor
-83 lines (70 sloc)  3.02 KB
-  
 /*
 사용자 생성
 	CREATE USER C##NAMSK IDENTIFIED BY 1234;
@@ -50,12 +20,15 @@ SELECT * FROM USER_USERS;
 SELECT * FROM ALL_USERS;
 
 -- 로그인 권한 부여
+CREATE USER C##NAMSK IDENTIFIED BY 1234;   -- 로그인할 수 없는 상태
+-- 절절한 권한을 부여해야 한다ㅏ
 GRANT create session TO C##NAMSK;   --  C##NAMSK에게 세션 생성(로그인) 권한을 부여
 
 /* 로그인 해서 다음의 쿼리를 수행해 봅니다.
 CREATE TABLE test(a NUMBER); -- 권한 불충분
 */
 GRANT connect, resource TO C##NAMSK;    --  접속과 자원 접근 롤을 C##NAMSK에게 부여
+-- 일반 데이터베이스 사용자로 활용 가능
 
 /* 다시 로그인 해서 다음의 쿼리를 수행해 봅니다.
 CREATE TABLE test(a NUMBER); -- 테이블이 생성
@@ -105,21 +78,113 @@ SELECT * FROM role_sys_privs WHERE role='RESOURCE';
 
 SHOW USER;
 -- System 계정으로 진행
--- HR 계정의 employees 테이블의 조회 권한을 C##NAMSK에게 부여하고 싶다면
-GRANT SELECT ON hr.employees TO C##NAMSK;
+-- HR 계정의 employees 테이블의 조회 권한을 C##NAMSK에게 부여하고 싶다면(system)
+GRANT SELECT ON hr.employees TO C##NAMSK;   --  권한의 부여
+REVOKE SELECT ON hr.employees FROM C##NAMSK;    -- 권한의 회수
 
 -- C##NAMSK로 진행
 SHOW USER;
 SELECT * FROM hr.employees; --  hr.employees의 SELECT 권한을 부여받았으므로 테이블 조회 가능
-? 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
+----------
+-- DDL
+----------
+
+-- 내가 가진 table 확인
+SELECT * FROM tab;
+-- 테이블의 구조 확인
+DESC test;
+
+-- 테이블 삭제
+DROP TABLE test;
+SELECT * FROM tab;
+--  휴지통
+PURGE RECYCLEBIN;   -- 삭제된 테이블은 휴지통에 보관
+
+SELECT * FROM tab;
+
+-- CREATE TABLE
+CREATE TABLE book ( -- 컬럼 명세
+    book_id NUMBER(5),  --  5자리 숫자
+    title VARCHAR2(50), --  50글자 가변문자
+    author VARCHAR2(10),    -- 10글자 가변문자열
+    pub_date DATE DEFAULT SYSDATE   -- 기본값은 현재시간
+);
+DESC book;
+
+-- 서브쿼리를 활용한 테이블 생성
+-- hr.employees 테이블을 기반으로 일부 데이터를 추출
+--  새 테이블
+SELECT * FROM hr.employees WHERE job_id like 'IT_%';
+CREATE TABLE it_emps AS (
+    SELECT * FROM hr.employees WHERE job_id like 'IT_%'
+);
+
+SELECT * FROM it_emps;
+CREATE TABLE emp_summary AS (
+    SELECT employee_id, 
+        first_name || ' ' || last_name full_name,
+        hire_date, salary
+    FROM hr.employees
+);
+DESC emp_summary;
+SELECT * FROM emp_summary;
+
+-- author 테이블 
+DESC book;
+CREATE TABLE author (
+    author_id NUMBER(10),
+    author_name VARCHAR2(100) NOT NULL, -- NULL일 수 없다
+    author_desc VARCHAR2(500),
+    PRIMARY KEY (author_id) -- author_id 컬럼을 PK로
+);
+DESC author;
+
+-- book테이블에 author 테이블 연결을 위해
+-- book 테이블의 author 컬럼을 삭제: DROP COLUMN
+ALTER TABLE book
+DROP COLUMN author;
+DESC book;
+
+-- author테이블 참조를 위한 author_id 컬럼을 book에 추가
+ALTER TABLE book
+ADD (author_id NUMBER(10));
+DESC book;
+
+-- book 테이블의 PK로 사용할 book_id도 NUMBER(10)으로 변경
+ALTER TABLE book
+MODIFY (book_id NUMBER(10));
+DESC book;
+
+-- 제약조건의 추가: ADD CONSTRAINT
+-- book 테이블의 book_id를 PRIMARY KEY 제약조건 부여
+ALTER TABLE book
+ADD CONSTRAINT pk_book_id PRIMARY KEY(book_id);
+DESC book;
+
+-- FOREIGN KEY 추가
+-- book 테이블의 author_id가 author의 author_id를 참조
+ALTER TABLE book
+ADD CONSTRAINT
+    fk_author_id FOREIGN KEY (author_id)
+        REFERENCES author(author_id);
+DESC book;
+
+-- COMMENT
+COMMENT ON TABLE book IS 'Book Information';
+COMMENT ON TABLE author IS 'Author Information';
+
+-- 테이블 커멘트 확인
+SELECT * FROM user_tab_comments;
+SELECT comments FROM user_tab_comments 
+WHERE table_name='BOOK';
+
+--  Data Dictionary
+-- Oracle은 내부에서 발생하는 모든 정로를 Data Dictionary에 담아두고 있다
+--  계정별로 USER_(일반 사용자), ALL_(전체 사용자), DBA_(관리자 전용) 접근 범위를 제한함
+-- 모든 딕셔너리 확인
+SHOW user;
+SELECT * FROM dictionary;
+
+-- DBA_ 딕셔너리 확인
+-- dba로 로그인 필요 as sysdba
